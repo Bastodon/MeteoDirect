246969a021fa13fba6741cb8ea8d45369fc6d658
Bastodon <superbastodon@gmail.com>
2024-02-27 17:51:22 +0100

    Mise à jour de la map. Ajout de la fonction pour récupérer la température de chaque région. La partie FRONT est terminée. Il ne reste plus qu'à réaliser une solution BACK pour comparer et calculer la température la plus basse parmi toutes les régions.

--- meteo.js
+++ meteo.js
@@ -2,12 +2,26 @@ var callBackGetSuccess = function(data) {
     console.log("donnees API", data)
     //alert("Il fait " + data.main.temp + "°C en Bretagne");
     var element = document.getElementById("zone_meteo");
-    element.innerHTML = data.name + " : " + data.main.temp;
+    element.innerHTML = data.name + " : " + data.main.temp + " °C";
 }
 
 function buttonClickGET(){
     var queryLoc = document.getElementById("queryLoc").value;
     var url = "https://api.openweathermap.org/data/2.5/weather?q="+queryLoc+"&appid=6d6fcda6e2a66bd3ec44f01af669fd2b&units=metric"
+
+    $.get(url, callBackGetSuccess).done(function() {
+        //alert("second success" );
+    })
+    .fail(function() {
+        alert(" erreur ");
+    })
+    .always(function() {
+        //alert( "finished" );
+    });
+}
+
+function mapClickGET(city){
+    var url = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=6d6fcda6e2a66bd3ec44f01af669fd2b&units=metric"
 
     $.get(url, callBackGetSuccess).done(function() {
         //alert("second success" );
