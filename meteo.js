229cb89e6a1b7474e11b96bf273fd0203a983c7b
Bastodon <superbastodon@gmail.com>
2024-02-27 17:02:38 +0100

    Import des travaux de meteo-france, premiers tests de l'API OpenWeather

--- meteo.js
+++ meteo.js
@@ -1,13 +1,21 @@
 var callBackGetSuccess = function(data) {
-
+    console.log("donnees API", data)
+    //alert("Il fait " + data.main.temp + "°C en Bretagne");
+    var element = document.getElementById("zone_meteo");
+    element.innerHTML = data.name + " : " + data.main.temp;
 }
 
 function buttonClickGET(){
-    var url = "https://api.openweathermap.org/data/2.5/weather?lat=48.2640845&lon=-2.9202408&appid=6d6fcda6e2a66bd3ec44f01af669fd2b"
+    var queryLoc = document.getElementById("queryLoc").value;
+    var url = "https://api.openweathermap.org/data/2.5/weather?q="+queryLoc+"&appid=6d6fcda6e2a66bd3ec44f01af669fd2b&units=metric"
 
     $.get(url, callBackGetSuccess).done(function() {
         //alert("second success" );
-
-
+    })
+    .fail(function() {
+        alert(" erreur ");
+    })
+    .always(function() {
+        //alert( "finished" );
     });
 }
