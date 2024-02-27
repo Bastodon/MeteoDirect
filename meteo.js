031610c552f9c56a4e57458eb309d11db698760a
Bastodon <superbastodon@gmail.com>
2024-02-27 18:13:45 +0100

    Fin de test de l'upload sur Netlify. Objectif : réaliser le calcul en BACK java

--- meteo.js
+++ meteo.js
@@ -7,13 +7,13 @@ var callBackGetSuccess = function(data) {
 
 function buttonClickGET(){
     var queryLoc = document.getElementById("queryLoc").value;
-    var url = "https://api.openweathermap.org/data/2.5/weather?q="+queryLoc+"&appid=6d6fcda6e2a66bd3ec44f01af669fd2b&units=metric"
+    var url = "https://api.openweathermap.org/data/2.5/weather?q="+queryLoc+"&lang=fr&appid=6d6fcda6e2a66bd3ec44f01af669fd2b&units=metric"
 
     $.get(url, callBackGetSuccess).done(function() {
         //alert("second success" );
     })
     .fail(function() {
-        alert(" erreur ");
+        alert(" Erreur : les coordonnées entrées ne sont pas reconnues. Vérifiez l'orthographe du nom de la ville et assurez-vous d'avoir indiqué le bon pays (fr, en, usa, etc...) ");
     })
     .always(function() {
         //alert( "finished" );
@@ -21,7 +21,7 @@ function buttonClickGET(){
 }
 
 function mapClickGET(city){
-    var url = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=6d6fcda6e2a66bd3ec44f01af669fd2b&units=metric"
+    var url = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&lang=fr&appid=6d6fcda6e2a66bd3ec44f01af669fd2b&units=metric"
 
     $.get(url, callBackGetSuccess).done(function() {
         //alert("second success" );
